2e271688027eae0e6c273a0b34df48c76c0fe18c
Safak Turkeli <sfktrkl@gmail.com>
2023-06-13 03:39:50 +0300

    Tasks with Return Values

---
+++ ParallelProgramming/TaskProgramming/Creating and Starting Tasks/TasksWithReturnValues.cs
@@ -0,0 +1,30 @@
+using Xunit;
+
+using System;
+using System.Threading.Tasks;
+
+namespace TaskProgramming;
+
+public partial class CreatingStartingTasks
+{
+    public static int? TextLength(object? o)
+    {
+        Console.WriteLine($"Task with id {Task.CurrentId} processing object '{o}'...");
+        return o?.ToString()?.Length;
+    }
+
+    [Fact]
+    public void TasksWithReturnValues()
+    {
+        string text1 = "testing";
+        string text2 = "this";
+
+        var task1 = new Task<int?>(TextLength, text1);
+        task1.Start();
+        var task2 = Task.Factory.StartNew(TextLength, text2);
+
+        // Getting the result is a blocking operation!
+        Console.WriteLine($"Length of '{text1}' is {task1.Result}.");
+        Console.WriteLine($"Length of '{text2}' is {task2.Result}.");
+    }
+}
